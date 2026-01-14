dae773d55eb71815e3803ff3616888d3cb8de567
Sarthak Bhagia <sarthakbhagia1@gmail.com>
2026-01-14 23:35:27 +0530

    Create A-BoyorGirl.cpp

---
+++ A-BoyorGirl.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    s.erase(unique(s.begin(), s.end()), s.end());
+    if (s.size() % 2 == 0) {    
+      cout << "CHAT WITH HER!\n";
+    } 
+    else {
+      cout << "IGNORE HIM!\n";
+    }
+    return 0;
+    }
