ab94c22aee52cc793b9295dabfe54ef6b5bc79cf
Sarthak Bhagia <sarthakbhagia1@gmail.com>
2026-01-18 00:18:30 +0530

    Create 41A-Translation.cpp

---
+++ 41A-Translation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    string t;
+    cin>>t;
+    reverse(t.begin(),t.end());
+    if(s==t){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
